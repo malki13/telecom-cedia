73d3626aa46cd84411064526e45ac45bd92d617e
malki13 <malkiyupanki12@gmail.com>
2024-02-22 17:27:02 -0500

    actualizacion servicios

---
+++ auth-service/src/main/java/com/tutorial/authservice/dto/TokenDto.java
@@ -0,0 +1,30 @@
+package com.tutorial.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class TokenDto {
+
+    private String token;
+
+    public TokenDto() {
+    }
+
+    public TokenDto(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
